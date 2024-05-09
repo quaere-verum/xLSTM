b309ac43bf2c876ae0bb61f2ac3f13f8a8a297ca
quaere-verum <147748435+quaere-verum@users.noreply.github.com>
2024-05-09 09:58:30 +0200

    Update xlstm.py
    Combined the linear and multilinear computations into one block which can subsequently be chunked into the parameters for the gates.

--- xlstm.py
+++ xlstm.py
@@ -20,12 +20,10 @@ class xLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, embedding_dim, device=th.device('cuda')):
         super().__init__()
         self.device = device
-        self.output_gate = MultiLinear((hidden_size, input_size, embedding_dim), 'bi, lij -> blj', bias_shape=(hidden_size, embedding_dim)).to(self.device)
-        self.forget_gate = nn.Linear(input_size, hidden_size).to(self.device)
-        self.input_gate = nn.Linear(input_size, hidden_size).to(self.device)
-        self.value = MultiLinear((hidden_size, input_size, embedding_dim), 'bi, lij -> blj', bias_shape=(hidden_size, embedding_dim)).to(self.device)
-        self.key = MultiLinear((hidden_size, input_size, embedding_dim), 'bi, lij -> blj', bias_shape=(hidden_size, embedding_dim)).to(self.device)
-        self.query = MultiLinear((hidden_size, input_size, embedding_dim), 'bi, lij -> blj', bias_shape=(hidden_size, embedding_dim)).to(self.device)
+        # output, value, key, query
+        self.multilinear_gates = MultiLinear((hidden_size*4, input_size, embedding_dim), 'bi, lij -> blj', bias_shape=(hidden_size*4, embedding_dim)).to(self.device)
+        # input, forget
+        self.linear_gates = nn.Linear(input_size, hidden_size*2).to(self.device)
         
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -47,13 +45,12 @@ class xLSTM(nn.Module):
         
     @th.jit.export
     def _forward_step(self, input):
-        o = F.sigmoid(self.output_gate(input))
-        f_ = self.forget_gate(input)
-        i_ = self.input_gate(input)
-        v = self.value(input)
-        k = self.key(input)/self.scale
-        q = self.query(input)
-        m = th.amax(th.stack((f_ + self.m, i_), dim=-1), dim=-1)
+        linear_gates = self.linear_gates(input)
+        multilinear_gates = self.multilinear_gates(input)
+        o_, v, k, q = th.chunk(multilinear_gates, 4, dim=1)
+        o = F.sigmoid(o_)
+        i_, f_ = th.chunk(linear_gates, 2, dim=1)
+        m = th.amax(th.stack((f_ + self.m, i_), dim=2), dim=2)
         i = th.exp(i_ - m)
         f = th.exp(f_ + self.m - m)
         self.m = m
@@ -67,3 +64,9 @@ class xLSTM(nn.Module):
         for t in range(input.shape[1]):
             self._forward_step(input[:, t, :])
         return self.hidden_state, self.cell_state
+
+if __name__ == '__main__':
+    model = xLSTM(17, 19, 23)
+    compiled_model = th.jit.script(model)
+    output = compiled_model(th.randn(7, 5, 17))
+    print(output[0].shape, output[1].shape)
